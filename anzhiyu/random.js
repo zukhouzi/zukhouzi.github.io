2e70726428177088be9d8253651dd79c1bb94d0b
mougik <mougik@qq.com>
2024-03-27 11:00:29 +0800

    Site updated: 2024-03-27 11:00:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/c89c.html","articles/3eeb.html","articles/82cf.html"];function toRandomPost(){
+var posts=["articles/c89c.html","articles/f7f.html","articles/3eeb.html","articles/82cf.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
