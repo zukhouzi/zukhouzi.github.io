d388b63a41c6cb9b95b7e640f50bcf1d2e4904be
mougik <mougik@qq.com>
2024-05-12 10:08:05 +0800

    Site updated: 2024-05-12 10:08:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/c89c.html","articles/f7f.html","articles/3eeb.html","articles/82cf.html","articles/9cfe.html","articles/2db2.html","articles/ff8a.html"];function toRandomPost(){
+var posts=["articles/c89c.html","articles/f7f.html","articles/3eeb.html","articles/82cf.html","articles/9cfe.html","articles/2db2.html","articles/ff8a.html","articles/bb30.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
